59917fd605ab7dfc494b7c43feb27c9b2398f005
Alberto Aragón Alvarez <ronin8503@gmail.com>
2025-03-13 16:10:27 +0000

    query variables

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 // db
-import db from './_db.js'
+import db from './_db.js';
 
 //types
-import { typeDefs } from './schema.js'
+import { typeDefs } from './schema.js';
 
 // server setup
 const serverPort = 4000;
@@ -13,29 +13,37 @@ const serverPort = 4000;
 const resolvers = {
   Query: {
     games() {
-      return db.games
+      return db.games;
     },
     reviews() {
-      return db.reviews
+      return db.reviews;
     },
     authors() {
-      return db.authors
-    }
-  }
-}
+      return db.authors;
+    },
+    review(_, args, context) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    game(_, args, context) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    author(_, args, context) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+};
 
 const server = new ApolloServer({
   // typeDefs -- definitions of types of data
   typeDefs,
   // resolvers -- describes the shape of the graph and the data in then including relationships between data objects
-  resolvers
-
-})
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: {
-    port: serverPort
-  }
-})
+    port: serverPort,
+  },
+});
 
-console.log('Server ready at port: ', serverPort)
+console.log('Server ready at port: ', serverPort);
